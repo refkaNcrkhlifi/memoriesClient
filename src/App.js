0acca3489ad718029d1d4018e617a0778cc93bd5
refkaNcrkhlifi <refka.khlifi11@gmail.com>
2021-05-09 13:51:58 +0100

    add style for mobile devices

--- src/App.js
+++ src/App.js
@@ -18,12 +18,12 @@ function App() {
     return (
         <Container maxWidth="lg">
             <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center"> Memories</Typography>
+                <Typography className={classes.heading} variant="h2" align="center" > Memories</Typography>
                 <img className={classes.image} src={memories} height="60" />
             </AppBar>
             <Grow in>
                 <Container>
-                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                    <Grid container className={classes.mainContainer} justify="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7} >
                             <Posts curentPostId={curentPostId} setCurentPostId={setCurentPostId} />
                         </Grid>
