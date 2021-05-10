7299cad4fad56866a2d9f4dbba9c6db70c2e6c8b
refkaNcrkhlifi <refka.khlifi11@gmail.com>
2021-05-10 14:31:02 +0100

    make code better

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,5 @@
+export const FETCH_POSTES='FETCH_POSTES';
+export const ADD_POST='ADD_POST';
+export const UPDATE_POST='UPDATE_POST';
+export const DELETE_POST='DELETE_POST';
+export const LIKE_POST='LIKE_POST';
