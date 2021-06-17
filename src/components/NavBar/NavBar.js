419f6c3cce4b5b8b06ba13c8b197dc977a09a1c9
refkaNcrkhlifi <refka.khlifi11@gmail.com>
2021-06-17 07:08:54 +0100

    add final touch

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,6 +4,7 @@ import { Link, useHistory, useLocation } from "react-router-dom";
 import memories from '../../images/memories.png';
 import useStyles from './styles';
 import { useDispatch } from 'react-redux';
+import decode from 'jwt-decode';
 import { LOGOUT } from '../../constants/actionTypes';
 
 export default function NavBar() {
@@ -12,22 +13,26 @@ export default function NavBar() {
     const dispatch = useDispatch()
     const location = useLocation()
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')))
-    useEffect(() => {
-        // const token = user?.token
 
-
-        setUser(JSON.parse(localStorage.getItem('profile')))
-    }, [location])
     const hundelLogOut = () => {
         setUser(null)
         dispatch({ type: LOGOUT })
         history.push('/auth')
     }
+    useEffect(() => {
+        const token = user?.token
+        if (token) {
+            const decodedToken = decode(token)
+            if (decodedToken.exp * 1000 < new Date().getTime()) hundelLogOut()
+        }
+
+        setUser(JSON.parse(localStorage.getItem('profile')))
+    }, [location])
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
             <div className={classes.brandContainer}>
-                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center" > Memories</Typography>
                 <img className={classes.image} src={memories} alt="icon" height="60" />
+                <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center" > Memories</Typography>
             </div>
             <Toolbar className={classes.toolbar}>
                 {
--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
-import ThumbUpAlt from '@material-ui/icons/ThumbUpAlt';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
@@ -10,6 +11,14 @@ import { deletePost, likePost } from '../../../actions/posts';
 function Post({ post, curentPostId, setCurentPostId }) {
     const dispatch = useDispatch()
     const classes = useStyles()
+    const user = JSON.parse(localStorage.getItem('profile'))
+    const Likes = () => {
+        if (post.likes.length > 0)
+            return post.likes.filter(like => like === (user?.result?.googleId || post?.result?._id)) ? (
+                <><ThumbUpAltIcon fontSize="small" /> &nbsp;{post.likes.length > 2 ? `you and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}`} </>
+            ) : (<><ThumbUpAltOutlined fontSize="small" /> &nbsp;{post.likes.length}  ${post.likes.length > 2 ? `likes` : `like`} </>)
+        return <><ThumbUpAltOutlined fontSize="small" /> &nbsp; like &nbsp; </>
+    }
     return (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -18,11 +27,13 @@ function Post({ post, curentPostId, setCurentPostId }) {
                 <Typography variant="h6">{post.name}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
+            {
+                (user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
 
-            <div className={classes.overlay2}>
-                <Button onClick={() => { setCurentPostId(post._id) }} style={{ color: "white" }} size="small"><MoreHorizIcon fontSize="default" /></Button>
-            </div>
-
+                    <div className={classes.overlay2}>
+                        <Button onClick={() => { setCurentPostId(post._id) }} style={{ color: "white" }} size="small"><MoreHorizIcon fontSize="default" /></Button>
+                    </div>
+                )}
             <div className={classes.details}>
                 <Typography variant="body2" color="textSecondary" component="h2"> {post.tags.map((tag) => `#${tag} `)}</Typography>
             </div>
@@ -34,14 +45,17 @@ function Post({ post, curentPostId, setCurentPostId }) {
             </CardContent>
 
             <CardActions className={classes.cardActions}>
-                <Button onClick={() => { dispatch(likePost(post._id)) }} color="primary" size="small"><ThumbUpAlt fontSize="small" />
-                &nbsp; like &nbsp;{post.likes.length} </Button>
-
-                <Button onClick={() => {
-                    dispatch(deletePost(post._id))
-                }} color="primary" size="small">
-                    <DeleteIcon fontSize="small" />
+                <Button disabled={!user?.result} onClick={() => { dispatch(likePost(post._id)) }} color="primary" size="small"><Likes /> </Button>
+                {
+                    (user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+                        <Button onClick={() => {
+                            dispatch(deletePost(post._id))
+                        }} color="primary" size="small">
+                            <DeleteIcon fontSize="small" />
                     Delete</Button>
+                    )
+                }
+
             </CardActions>
 
         </Card>
